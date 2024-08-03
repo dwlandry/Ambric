965bbc19a02ffedf11449002f632bf22210a2f13
David Landry <david.landry@ambric.com>
2024-08-03 17:43:44 -0500

    Add ClearContactTasksController to manage Employee tasks
    Introduce ClearContactTasksController in Instrument_Spec_Manager.Module.Controllers to clear tasks associated with an Employee in DetailView. The controller includes a SimpleAction 'clearTasksAction' with a caption, image, tooltip, and confirmation message. The ClearTasksAction_Execute method removes all tasks from the Employee's DemoTasks collection. Overridden OnActivated, OnViewControlsCreated, and OnDeactivated methods provide additional hooks. Added partial class in ClearContactTasksController.Designer.cs for designer support.

---
+++ Instrument_Spec_Manager.Module/Controllers/ClearContactTasksController.cs
@@ -0,0 +1,67 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using Instrument_Spec_Manager.Module.BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Instrument_Spec_Manager.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class ClearContactTasksController : ViewController
+    {
+        // Use CodeRush to create Controllers and Actions with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/403133/
+        public ClearContactTasksController()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+            TargetViewType = ViewType.DetailView;
+            TargetObjectType = typeof(Employee);
+
+            SimpleAction clearTasksAction = new SimpleAction(this, "ClearTasks", PredefinedCategory.View);
+            clearTasksAction.Caption = "Clear Tasks";
+            clearTasksAction.Execute += ClearTasksAction_Execute;
+            clearTasksAction.ImageName = "Action_Clear";
+            clearTasksAction.ToolTip = "Clear all tasks for this contact";
+            clearTasksAction.ConfirmationMessage = "Are you sure you want to clear all tasks for this contact?";
+
+        }
+
+        private void ClearTasksAction_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            while (((Employee)View.CurrentObject).DemoTasks.Count > 0)
+            {
+                ((Employee)View.CurrentObject).DemoTasks.Remove(((Employee)View.CurrentObject).DemoTasks[0]);
+                ObjectSpace.SetModified(View.CurrentObject, View.ObjectTypeInfo.FindMember(nameof(Employee.DemoTasks)));
+            }
+
+        }
+
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
