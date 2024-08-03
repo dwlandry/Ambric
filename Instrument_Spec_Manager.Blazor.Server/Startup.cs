52cae2a449c72b53113f8982cd1c6545972b0aeb
David Landry <david.landry@ambric.com>
2024-08-03 15:56:45 -0500

    Refactor database config logic in Startup.cs
    Revised database configuration in `Startup.cs` to enhance flexibility:
    - Introduced conditional checks for connection strings.
    - Threw `ArgumentNullException` if no connection string is found.
    - Added `#if EASYTEST` directive for EasyTest environment:
      - Used in-memory database for testing when `EASYTEST` is defined.
      - Configured Npgsql for PostgreSQL otherwise.
    - Improved environment-based database configuration for better testing and production readiness.

--- Instrument_Spec_Manager.Blazor.Server/Startup.cs
+++ Instrument_Spec_Manager.Blazor.Server/Startup.cs
@@ -40,25 +40,29 @@ public class Startup {
             builder.ObjectSpaceProviders
                 .AddSecuredEFCore(options => options.PreFetchReferenceProperties())
                     .WithDbContext<Instrument_Spec_Manager.Module.BusinessObjects.Instrument_Spec_ManagerEFCoreDbContext>((serviceProvider, options) => {
-                        // Uncomment this code to use an in-memory database. This database is recreated each time the server starts. With the in-memory database, you don't need to make a migration when the data model is changed.
-                        // Do not use this code in production environment to avoid data loss.
-                        // We recommend that you refer to the following help topic before you use an in-memory database: https://docs.microsoft.com/en-us/ef/core/testing/in-memory
-                        //options.UseInMemoryDatabase("InMemory");
+                        
                         string connectionString = null;
                         if(Configuration.GetConnectionString("ConnectionString") != null) {
                             connectionString = Configuration.GetConnectionString("ConnectionString");
                         }
-#if EASYTEST
-                        if(Configuration.GetConnectionString("EasyTestConnectionString") != null) {
-                            connectionString = Configuration.GetConnectionString("EasyTestConnectionString");
-                        }
-#endif
+
                         ArgumentNullException.ThrowIfNull(connectionString);
-                        //options.UseSqlServer(connectionString);
-                        options.UseNpgsql(connectionString);
+                        //options.UseNpgsql(connectionString);
                         options.UseChangeTrackingProxies();
                         options.UseObjectSpaceLinkProxies();
                         options.UseLazyLoadingProxies();
+#if EASYTEST
+                        if (Configuration.GetConnectionString("EasyTestConnectionString") != null) {
+                            connectionString = Configuration.GetConnectionString("EasyTestConnectionString");
+                        }
+                        // Uncomment this code to use an in-memory database. This database is recreated each time the server starts. With the in-memory database, you don't need to make a migration when the data model is changed.
+                        // Do not use this code in production environment to avoid data loss.
+                        // We recommend that you refer to the following help topic before you use an in-memory database: https://docs.microsoft.com/en-us/ef/core/testing/in-memory
+                        options.UseInMemoryDatabase("InMemory");
+#else
+                        options.UseNpgsql(connectionString);
+#endif
+
                     })
                 .AddNonPersistent();
             builder.Security
