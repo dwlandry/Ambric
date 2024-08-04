094c2183c7560ad3649b63ba18de39de91300b23
David Landry <david.landry@ambric.com>
2024-08-03 21:58:09 -0500

    Add Note entity and PopupNotesController
    Introduce a new Note entity to the Instrument_Spec_Manager module, including properties and database table mapping. Create a migration file to handle the Notes table creation. Add PopupNotesController to manage note display and selection in a popup window. Include designer support for PopupNotesController. Update model snapshot to include the Note entity.

---
+++ Instrument_Spec_Manager.Module/Migrations/20240804025257_AddNote.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Instrument_Spec_Manager.Module.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Author = table.Column<string>(type: "text", nullable: true),
+                    DateTime = table.Column<DateTime>(type: "timestamp", nullable: true),
+                    Text = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
