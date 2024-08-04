d7ea8a2538300aa00601bbc18909cfbcc8b451c3
David Landry <david.landry@ambric.com>
2024-08-03 22:08:35 -0500

    Add TaskActionsController for managing task actions
    Introduced a new `TaskActionsController` class in the `Instrument_Spec_Manager.Module.Controllers` namespace to manage task-related actions. This controller inherits from `ViewController` and includes private fields for priority and status items. The constructor initializes the controller, sets the target object type to `DemoTask`, and creates a `SingleChoiceAction` named `SetTaskAction` for setting task properties.
    
    The `FillItemWithEnumValues` method populates action items with enum values using `EnumDescriptor`. Overridden methods `OnActivated`, `OnViewControlsCreated`, and `OnDeactivated` provide hooks for additional functionality during the controller's lifecycle. The `TaskActionsController.Designer.cs` file is updated to include the partial class definition and necessary designer-generated code.

---
+++ Instrument_Spec_Manager.Module/Controllers/TaskActionsController.cs
@@ -0,0 +1,78 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using Instrument_Spec_Manager.Module.BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Instrument_Spec_Manager.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class TaskActionsController : ViewController
+    {
+        private ChoiceActionItem setPriorityItem;
+        private ChoiceActionItem setStatusItem;
+
+        // Use CodeRush to create Controllers and Actions with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/403133/
+        public TaskActionsController()
+        {
+            InitializeComponent();
+            TargetObjectType = typeof(DemoTask);
+
+            SingleChoiceAction SetTaskAction = new SingleChoiceAction(this, "SetTaskAction", PredefinedCategory.Edit)
+            {
+                Caption = "Set Task",
+                //Specify the display mode for the Action's items. Here the items are operations that you perform against selected records.
+                ItemType = SingleChoiceActionItemType.ItemIsOperation,
+                //Set the Action to become available in the Task List View when a user selects one or more objects.
+                SelectionDependencyType = SelectionDependencyType.RequireMultipleObjects
+            };
+
+            setPriorityItem = new ChoiceActionItem(CaptionHelper.GetMemberCaption(typeof(DemoTask), "Priority"), null);
+            SetTaskAction.Items.Add(setPriorityItem);
+            FillItemWithEnumValues(setPriorityItem, typeof(Priority));
+
+            setStatusItem = new ChoiceActionItem(CaptionHelper.GetMemberCaption(typeof(DemoTask), "Status"), null);
+            SetTaskAction.Items.Add(setStatusItem);
+            FillItemWithEnumValues(setStatusItem, typeof(BusinessObjects.TaskStatus));
+        }
+
+        private void FillItemWithEnumValues(ChoiceActionItem parentItem, Type enumType)
+        {
+            EnumDescriptor ed = new EnumDescriptor(enumType);
+            foreach (object current in ed.Values)
+            {
+                ChoiceActionItem item = new ChoiceActionItem(ed.GetCaption(current), current);
+                item.ImageName = ImageLoader.Instance.GetEnumValueImageName(current);
+                parentItem.Items.Add(item);
+            }
+        }
+
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
