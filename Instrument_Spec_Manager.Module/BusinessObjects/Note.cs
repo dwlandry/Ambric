cec4dc48b355c2b9b74912c77a34ac2c6a299fb3
David Landry <david.landry@ambric.com>
2024-08-03 21:49:25 -0500

    Add Note class with DevExpress and EF integration
    Introduce a new class `Note` in the `Instrument_Spec_Manager.Module.BusinessObjects` namespace. This class includes properties for `ID`, `Author`, `DateTime`, and `Text`, each with specific attributes for DevExpress and Entity Framework integration. The `Note` class is designed to represent a note entity with attributes to control behavior in the application, including primary key designation, visibility control, and unlimited field size for the `Text` property.

---
+++ Instrument_Spec_Manager.Module/BusinessObjects/Note.cs
@@ -0,0 +1,31 @@
+﻿using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instrument_Spec_Manager.Module.BusinessObjects
+{
+    [DefaultProperty(nameof(Text))]
+    [ImageName("BO_Note")]
+    public class Note
+    {
+
+        [Key, Browsable(false)]
+        [DevExpress.ExpressApp.Data.Key]
+        [VisibleInDetailView(false), VisibleInListView(false), VisibleInLookupListView(false)]
+        public virtual Guid ID { get; set; }
+        public virtual String Author { get; set; }
+        
+        [Column(TypeName = "timestamp")]
+        public virtual DateTime? DateTime { get; set; }
+
+        [FieldSize(FieldSizeAttribute.Unlimited)]
+        public virtual String Text { get; set; }
+    }
+}
